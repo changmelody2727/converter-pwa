03540e5d65708ab6e565f4e9e38a7843b4963caf
Melody Chang <melody.chang@psg-solutions.com>
2024-03-16 18:26:11 -0600

    pwa added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import pkg from "next-pwa";
+
+const withPWA = pkg({
+  dest: "public",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
